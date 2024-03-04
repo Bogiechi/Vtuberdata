470210e1c55a600135a531be046e02f3c8807d85
Bogiechi <141477468+Bogiechi@users.noreply.github.com>
2024-03-04 10:49:20 +0800

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `vtuberdata`.`VtuberSC`(
+    `itemId` VARCHAR(50) NOT NULL,
+    `channelPlayCount` FLOAT NOT NULL,
+    `subscriberCount` FLOAT NOT NULL,
+    `subscriberFluc` FLOAT NOT NULL,
+    `maxLiveViewer` INT NOT NULL,
+    `donationAmount` FLOAT NOT NULL,
+    `donationCount` INT NOT NULL,
+    `rank` INT NOT NULL,
+    `rankFluc` VARCHAR(10) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+);
