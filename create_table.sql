adebb9da33bf6fa175de78b7367f55d9515a2122
Bogiechi <141477468+Bogiechi@users.noreply.github.com>
2024-04-17 22:18:03 +0800

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE `vtuberdata`.`VtuberSuperChat`(
     `channelPlayCount` FLOAT NOT NULL,
     `subscriberCount` FLOAT NOT NULL,
     `subscriberFluc` FLOAT NOT NULL,
-    `maxLiveViewer` INT NOT NULL,
+    `maxLiveViewer` VARCHAR(50) NOT NULL,
     `donationAmount` FLOAT NOT NULL,
     `donationCount` INT NOT NULL,
     `rank` INT NOT NULL,
